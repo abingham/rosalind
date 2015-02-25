f9157a43dae6ae05323f6bc64d0dd95d335dbaad
Austin Bingham <austin.bingham@gmail.com>
2015-02-25 16:38:02 +0100

    Added note.

--- wabbits.hs
+++ wabbits.hs
@@ -1,3 +1,5 @@
+
+-- This is actually super slow.
 wabbits factor = 1 : 1 : zipWith (\n0 n1 -> n0 * factor + n1) (wabbits factor) (drop 1 (wabbits factor))
 
 wabbitCount months factor = last $ take months $ wabbits factor
