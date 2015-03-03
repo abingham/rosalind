c097ced80854cf728f10db7c203022d5947db4c8
Austin Bingham <austin.bingham@gmail.com>
2015-03-03 20:39:55 +0100

    Inheritance may be working now...

--- inheritance/Inheritance.hs
+++ inheritance/Inheritance.hs
@@ -1,6 +1,9 @@
+import Data.Foldable
 import Data.Ratio
+import Data.Sequence
 
 data Gene = HD | H | HR
+          deriving (Show)
 
 type Population = (Int, Int, Int)
 
@@ -14,26 +17,36 @@ dominant _ H = 0.5
 dominant _ _ = 0
 
 -- |Calculate the probabilities of different parent genotype pairings
-pairing :: Gene -> Gene -> Population -> Maybe (Ratio Int)
-pairing HD HD pop@(k, m, n) = prob k (k - 1) pop
-pairing HD H  pop@(k, m, n) = prob k m pop
-pairing HD HR pop@(k, m, n) = prob k n pop
-pairing H HD pop@(k, m, n) = prob m k pop
-pairing H H  pop@(k, m, n) = prob m (m - 1) pop
-pairing H HR pop@(k, m, n) = prob m n pop
-pairing HR HD pop@(k, m, n) = prob n k pop
-pairing HR H  pop@(k, m, n) = prob n m pop
-pairing HR HR pop@(k, m, n) = prob n (n - 1) pop
+pairing :: Gene -> Gene -> Population -> Ratio Int
+pairing HD HD pop@(k, _, _) = prob k (k - 1) pop
+pairing HD H  pop@(k, m, _) = prob k m pop
+pairing HD HR pop@(k, _, n) = prob k n pop
+pairing H HD pop@(k, m, _) = prob m k pop
+pairing H H  pop@(_, m, _) = prob m (m - 1) pop
+pairing H HR pop@(_, m, n) = prob m n pop
+pairing HR HD pop@(k, _, n) = prob n k pop
+pairing HR H  pop@(_, m, n) = prob n m pop
+pairing HR HR pop@(_, _, n) = prob n (n - 1) pop
 
 -- |Calculate sub-probability for `pairing`.
-prob :: Int -> Int -> Population -> Maybe (Ratio Int)
+prob :: Int -> Int -> Population -> Ratio Int
 prob a b (m, n, o)
-  | size < 2 = Nothing
-  | otherwise = Just $ aProb * bProb
+  | size < 2 = 0 % 1
+  | otherwise = aProb * bProb
   where
-    size = m + n + 0 :: Int
+    size = m + n + o :: Int
     aProb = a % size :: Ratio Int
     bProb = b % (size - 1) :: Ratio Int
 
+probOfDominant :: Population -> Float
+probOfDominant pop = fromIntegral (numerator fullProb) / fromIntegral (denominator fullProb)
+  where
+    pairings = map toList $ replicateM 2 [HD, HR, H] :: [[Gene]]
+    pairingProbs = map (\[a, b] -> pairing a b pop) pairings :: [Ratio Int]
+    domProbs = map (\[a, b] -> dominant a b) pairings :: [Ratio Int]
+    fullProb = Prelude.sum $ Prelude.zipWith (*) pairingProbs domProbs
+
+main :: IO ()
 main = do
+  putStrLn $ show $ probOfDominant (2,2,2)
   return ()
