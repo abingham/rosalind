8ca84c3e41ea2cbe6f2d0cc16c8fabd381b27cb5
Austin Bingham <austinbingham@TK421.local>
2015-03-03 08:29:43 +0100

    Whatever.

--- inheritance/Inheritance.hs
+++ inheritance/Inheritance.hs
@@ -1,11 +1,11 @@
-import Data.List (tails)
+import Data.Ratio
 
 data Gene = HD | H | HR
 
 type Population = (Int, Int, Int)
 
 -- |Calculate probability of dominant phenotype given two genes
-dominant :: Gene -> Gene -> Float
+dominant :: Gene -> Gene -> Ratio Int
 dominant HD _ = 1
 dominant _ HD = 1
 dominant H H = 0.75
@@ -14,7 +14,7 @@ dominant _ H = 0.5
 dominant _ _ = 0
 
 -- |Calculate the probabilities of different parent genotype pairings
-pairing :: Gene -> Gene -> Population -> Maybe Float
+pairing :: Gene -> Gene -> Population -> Maybe (Ratio Int)
 pairing HD HD pop@(k, m, n) = prob k (k - 1) pop
 pairing HD H  pop@(k, m, n) = prob k m pop
 pairing HD HR pop@(k, m, n) = prob k n pop
@@ -25,11 +25,15 @@ pairing HR HD pop@(k, m, n) = prob n k pop
 pairing HR H  pop@(k, m, n) = prob n m pop
 pairing HR HR pop@(k, m, n) = prob n (n - 1) pop
 
-count (m, n, o) = m + n + o
-prob a b pop = (a / count) *  (b / (count - 1))
+-- |Calculate sub-probability for `pairing`.
+prob :: Int -> Int -> Population -> Maybe (Ratio Int)
+prob a b (m, n, o)
+  | size < 2 = Nothing
+  | otherwise = Just $ aProb * bProb
+  where
+    size = m + n + 0 :: Int
+    aProb = a % size :: Ratio Int
+    bProb = b % (size - 1) :: Ratio Int
 
 main = do
-  putStrLn $ show $ fromIntegral (length dominant) / fromIntegral (length full)
-  where
-    full = map produceDominant $ combinations 2 $ population (2, 2, 2)
-    dominant = filter (\x -> x) full
+  return ()
