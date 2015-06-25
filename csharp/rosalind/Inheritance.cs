adb6cb0387b199cd0d8c73c39099e64ae99deafe
Austin Bingham <austin.bingham@gmail.com>
2015-06-25 14:30:48 +0200

    Changed name Gene to GeneType

--- csharp/rosalind/Inheritance.cs
+++ csharp/rosalind/Inheritance.cs
@@ -6,15 +6,15 @@ namespace rosalind
 {
     public static class Inheritance
     {
-        public enum Gene {HD, H, HR};
+        public enum GeneType {HD, H, HR};
    
-        public class Population : Dictionary<Gene, uint> {
+        public class Population : Dictionary<GeneType, uint> {
             public Population() : base() {}
-            public Population(IDictionary<Gene, uint> d) : base(d) {}
+            public Population(IDictionary<GeneType, uint> d) : base(d) {}
         }
 
-        public static IEnumerable<Tuple<Gene, Gene>> allPairings() {
-            var genes = new List<Gene>(){ Gene.HD, Gene.H, Gene.HR};
+        public static IEnumerable<Tuple<GeneType, GeneType>> allPairings() {
+            var genes = new List<GeneType>(){ GeneType.HD, GeneType.H, GeneType.HR};
             foreach (var g1 in genes) {
                 foreach (var g2 in genes) {
                     yield return Tuple.Create (g1, g2);
@@ -22,22 +22,22 @@ namespace rosalind
             }
         }
 
-        static Rational dominantProb(Gene a, Gene b) {
-            if (a == Gene.HD || b == Gene.HD)
+        static Rational dominantProb(GeneType a, GeneType b) {
+            if (a == GeneType.HD || b == GeneType.HD)
                 return new Rational(1, 1);
-            else if (a == Gene.H && b == Gene.H)
+            else if (a == GeneType.H && b == GeneType.H)
                 return new Rational(3, 4);
-            else if (a == Gene.H || b == Gene.H)
+            else if (a == GeneType.H || b == GeneType.H)
                 return new Rational(1, 2);
             else
                 return new Rational(0, 1);
         }
 
-        public static Rational selectionProb(Gene g, Population pop) {
+        public static Rational selectionProb(GeneType g, Population pop) {
             return new Rational(pop [g], pop.Values.Aggregate((a, b) => a + b));
         }
 
-        static Rational pairingProb(Gene a, Gene b, Population pop) {
+        static Rational pairingProb(GeneType a, GeneType b, Population pop) {
             var subPop = new Population (pop);
             subPop [a] -= 1;
             return selectionProb (a, pop) * selectionProb (b, subPop);
@@ -53,9 +53,9 @@ namespace rosalind
 
         public static Rational dominantProbability(uint hdCount, uint hCount, uint hrCount) {
             Population pop = new Population(){
-                {Gene.HD, hdCount},
-                {Gene.H, hCount}, 
-                {Gene.HR, hrCount}
+                {GeneType.HD, hdCount},
+                {GeneType.H, hCount}, 
+                {GeneType.HR, hrCount}
             };
             return pairingProbs (pop).Zip (dominantProbs (), (p1, p2) => p1 * p2).Aggregate ((rslt, item) => rslt + item);
         }
