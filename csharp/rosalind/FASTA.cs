c4e158ea01da587f16da17eacddc7f076f9932e8
Austin Bingham <austin.bingham@gmail.com>
2015-06-25 14:29:31 +0200

    Added independent FASTA parser

---
+++ csharp/rosalind/FASTA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace rosalind
+{
+    static public class FASTA
+    {
+        /*
+         * Parse a text stream in rosalind's FASTA format, generating a stream of (tag,dna) tuples.
+         */
+        static public IEnumerable<Tuple<string, string>> read(TextReader input)
+        {
+            string tag = "";
+            string dna = "";
+            string line;
+            while ((line = input.ReadLine ()) != null) {
+                line = line.Trim ();
+                if (line.Length == 0)
+                    continue;
+
+                if (line.StartsWith (">")) {
+                    line = line.Substring (1);
+                    if (tag.Length != 0) {
+                        yield return Tuple.Create (tag, dna);
+                    }
+                    tag = line;
+                    dna = "";
+                } else {
+                    dna += line;
+                }
+            }
+
+            if (tag.Length != 0) {
+                yield return Tuple.Create (tag, dna);
+            }
+        }
+    }
+}
+
