a9bb036efc812626366239bc1f6f0591b6d31ac7
Austin Bingham <austin.bingham@gmail.com>
2015-06-29 10:22:27 +0200

    Cleaning things up.

--- elixir/rosalind/count_nucleotides.exs
+++ elixir/rosalind/count_nucleotides.exs
@@ -1,4 +1,6 @@
+import Rosalind.Nucleotides
+
 filename = Enum.fetch!(System.argv, 0)
 data = to_string(File.read!(filename))
-counts = Rosalind.Nucleotides.count_nucleotides(String.strip(data))
-IO.puts "#{counts[:A]} #{counts[:C]} #{counts[:G]} #{counts[:T]}"
+%{:A => a, :C => c, :G => g, :T => t} = count_nucleotides(String.strip(data))
+IO.puts "#{a} #{c} #{g} #{t}"
