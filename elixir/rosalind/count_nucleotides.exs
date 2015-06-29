57dbb82d8fd0382f4624f287bcad0feeedeef964
Austin Bingham <austin.bingham@gmail.com>
2015-06-29 10:09:40 +0200

    Cleaned up nucleotide executable.

--- elixir/rosalind/count_nucleotides.exs
+++ elixir/rosalind/count_nucleotides.exs
@@ -1,6 +1,4 @@
 filename = Enum.fetch!(System.argv, 0)
-{:ok, file} = File.open filename, [:read]
-data = IO.read file, :all
-File.close file
-counts = Rosalind.Nucleotides.count_nucleotides String.strip(data)
+data = to_string(File.read!(filename))
+counts = Rosalind.Nucleotides.count_nucleotides(String.strip(data))
 IO.puts "#{counts[:A]} #{counts[:C]} #{counts[:G]} #{counts[:T]}"
